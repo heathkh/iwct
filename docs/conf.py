3e7c05ef70fd9ae77bb87a9ef4cb9bbda305069a
ubuntu <ubuntu@ip-10-66-13-226.ec2.internal>
2013-06-13 06:22:26 +0000

    Adds guards for missing theme.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,13 @@
 # serve to show the default.
 
 import sys, os
+
+has_bootstrap_theme = False
+try:
 import sphinx_bootstrap_theme
+has_bootstrap_theme = True
+except:
+  pass
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -96,8 +102,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = "nature"
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+if has_bootstrap_theme:
+  html_theme = 'bootstrap'
+  html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
